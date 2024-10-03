a15e834e70f91743b6f0f1d30efb8133051853e4
Gourav Sharma <gouravsharma2806@gmail.com>
2024-10-03 15:09:11 +0530

    login responsive

--- Frontend/src/comp/Login.jsx
+++ Frontend/src/comp/Login.jsx
@@ -49,38 +49,42 @@ const Login = () => {
       style={{ backgroundImage: 'url("")' }}
     >
       <ToastContainer /> {/* Add ToastContainer */}
-      <div className="flex-1 flex flex-col justify-center p-10 text-gray-900">
-        <h1 className="text-5xl font-bold mb-5">Welcome To Hi-Tech</h1>
-        <p className="text-lg mb-10">
+      <div className="flex-1 flex flex-col justify-center p-5 md:p-10 lg:p-16 text-gray-900">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-5">
+          Welcome To Hi-Tech
+        </h1>
+        <p className="text-sm md:text-lg mb-8 lg:mb-10">
           It is a long established fact that a reader will be distracted by the
           readable content of a page when looking at its layout.
         </p>
-        <div className="flex space-x-5 ml-10">
+        <div className="flex space-x-3 md:space-x-5 ml-5 md:ml-10">
           <a
             href="https://www.instagram.com/accounts/login/?hl=en"
-            className="bg-gray-700 p-4 rounded-full text-white hover:bg-orange-500 transition-all"
+            className="bg-gray-700 p-3 md:p-4 rounded-full text-white hover:bg-orange-500 transition-all"
           >
             <FaFacebookF />
           </a>
           <a
             href="https://www.instagram.com/accounts/login/?hl=en"
-            className="bg-gray-700 p-4 rounded-full text-white hover:bg-orange-500 transition-all"
+            className="bg-gray-700 p-3 md:p-4 rounded-full text-white hover:bg-orange-500 transition-all"
           >
             <FaTwitter />
           </a>
           <a
             href="https://www.instagram.com/accounts/login/?hl=en"
-            className="bg-gray-700 p-4 rounded-full text-white hover:bg-orange-500 transition-all"
+            className="bg-gray-700 p-3 md:p-4 rounded-full text-white hover:bg-orange-500 transition-all"
           >
             <FaInstagram />
           </a>
         </div>
       </div>
-      <div className="flex-1 flex flex-col justify-center p-16 bg-white bg-opacity-80">
-        <div className="max-w-md mx-auto">
-          <h2 className="text-3xl font-bold mb-8">Sign in</h2>
+      <div className="flex-1 flex flex-col justify-center p-8 md:p-16 bg-white bg-opacity-80">
+        <div className="max-w-xs md:max-w-md mx-auto">
+          <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-6 md:mb-8">
+            Sign in
+          </h2>
           <select
-            className="w-full p-4 mb-4 border border-gray-300 rounded-md focus:border-blue-500 focus:ring focus:ring-blue-200"
+            className="w-full p-3 md:p-4 mb-4 border border-gray-300 rounded-md focus:border-blue-500 focus:ring focus:ring-blue-200"
             value={selectedRole}
             onChange={(e) => setSelectedRole(e.target.value)}
           >
@@ -90,14 +94,14 @@ const Login = () => {
           </select>
           <input
             type="text"
-            className="w-full p-4 mb-4 border border-gray-300 rounded-md focus:border-blue-500 focus:ring focus:ring-blue-200"
+            className="w-full p-3 md:p-4 mb-4 border border-gray-300 rounded-md focus:border-blue-500 focus:ring focus:ring-blue-200"
             placeholder="User ID"
             value={userId}
             onChange={(e) => setUserId(e.target.value)}
           />
           <input
             type="password"
-            className="w-full p-4 mb-4 border border-gray-300 rounded-md focus:border-blue-500 focus:ring focus:ring-blue-200"
+            className="w-full p-3 md:p-4 mb-4 border border-gray-300 rounded-md focus:border-blue-500 focus:ring focus:ring-blue-200"
             placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
@@ -109,7 +113,7 @@ const Login = () => {
             </label>
           </div>
           <button
-            className="w-full p-4 bg-orange-500 text-white rounded-md hover:bg-orange-600 transition"
+            className="w-full p-3 md:p-4 bg-orange-500 text-white rounded-md hover:bg-orange-600 transition"
             onClick={handleLogin}
           >
             Sign in now
